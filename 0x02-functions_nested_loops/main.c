5d522cfa34192a8b46d4abfbb7a20260d71685aa
Thomas-picki <thomasendalemaw@gmail.com>
2022-03-15 22:50:48 +0300

    9th task commit

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,16 +1,23 @@
-cat main.h
-cat main.h
-#ifndef MAIN_H
-#define MAIN_H
-int _putchar(char c);
-void print_alphabet_x10(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-void jack_bauer(void);
-void times_table(void);
-int add (int, int);
-void print_to_98(int n);
-#endif
+#include <stdio.h>                                                                                 
+
+/**                                                                                                
+ * main - debugging example                                                                        
+ * Return: 0                                                                                       
+ */                                                                                                
+int main(void)                                                                                     
+{                                                                                                  
+        int i;                                                                                     
+        int j;                                                                                     
+
+        for (i = 0; i < 10; i++)                                                                   
+        {                                                                                          
+                j = 0;                                                                             
+                while (j < 10)                                                                     
+                {                                                                                  
+                        printf("%d", j);                                                           
+                }                                                                                  
+                printf("\n");                                                                      
+        }                                                                                          
+
+        return (0);                                                                                
+}             
