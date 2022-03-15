96f36c001f47f5486cdfdb98d11f5ef67ba98049
Thoma-picki <thomasendalemaw@gmail.com>
2022-03-15 10:25:19 -0700

    first task commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,13 @@
-#include "main.h"
+#include <stdio.h>
 /**
 * main - check the code
-* 
+*
 * Return: Always 0.
 */
-int main(void)
+int main()
 {
-print_alphabet();
-return (0);
+int ch;
+for(ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+return 0;
 }
