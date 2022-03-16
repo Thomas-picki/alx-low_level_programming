a89335c11dfc4435737a8dc761a403aa13be6b6e
Thomas-picki <thomasendalemaw@gmail.com>
2022-03-16 13:09:22 +0300

    9th task commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,48 +1,47 @@
-#include "main.h"
+#include "main.h"
 /**
- * print_times_table - prints the times table
- * @n: takes in an integer
+ * times_table -prints tables
+ *
+ * Return: Always 0.
  */
-void print_times_table(int n)
+void times_table(void)
 {
- int i, j, k;
-
- if (n > 15 || n < 0)
-  return;
- for (i = 0; i <= n; i++)
- {
-  for (j = 0; j <= n; j++)
-  {
-   k = i * j;
-   if (j == 0)
-   {
-    _putchar(k + '0');
-   }
-   else if (k <= 9)
-   {
-    _putchar(',');
-    _putchar(' ');
-    _putchar(' ');
-    _putchar(' ');
-    _putchar(k + '0');
-   }
-   else if (k >= 10 && k < 100)
-   {
-    _putchar(',');
-    _putchar(' ');
-    _putchar(' ');
-    _putchar(k / 10 + '0');
-    _putchar(k % 10 + '0');
-   }
-   else
-   {
-    _putchar(',');
-    _putchar(' ');
-    _putchar((k / 100) + '0');
-    _putchar(k / 10 % 10 + '0');
-    _putchar(k % 10 + '0');
-   }
-  }
-  _putchar('\n');
- }
+int a;
+int b;
+int c;
+for (a = 0; a <= 9; a++)
+{
+for (b = 0; b <= 9; b++)
+{
+c = a * b;
+if ((c / 10) == 0)
+{
+if (b == 0)
+{
+_putchar ('0');
+}
+if (b != 0)
+{
+_putchar (' ');
+_putchar ((c % 10) + '0');
+}
+if (b < 9)
+{
+_putchar(',');
+_putchar (' ');
+}
+}
+else
+{
+_putchar ((c / 10) + '0');
+_putchar ((c % 10) + '0');
+if (b < 9)
+{
+_putchar(',');
+_putchar (' ');
+}
+}
+}
+_putchar ('\n');
+}
 }
