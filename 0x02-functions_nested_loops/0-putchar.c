16e9c7d021da3144230ab9830c4530d139f91a39
Thomas-picki <thomasendalemaw@gmail.com>
2022-03-16 12:44:54 +0300

    o task commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,23 @@
 #include "main.h"
+
+
 /**
-* main -check the code
-* 
-* Return: always 0
-*/
+ * main - Entry point
+ *
+ * Return: nothing, return void
+ */
+
 int main(void)
 {
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
 }
