a00b69c295ec1906e9ba340d788a64832ffe12f0
Thomas-picki <thomasendalemaw@gmail.com>
2022-03-17 23:16:00 +0330

    2nd task commit

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -15,12 +15,12 @@ int largest;
 if (a >= b && a>=c)
 {
 largest = a;
-} 
+}
 else if (b >= a && b >=c)
 {
 largest = b;
 }
-else 
+else
 {
 largest =c;
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,12 @@
+$ cat > main.h
+$ cat main.h
 #ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
 
 void posistive_or_negative(int i);
-int largest_number(int a, int , int c);
+int largest_number(int a, int b, int c);
 void print_remaining(int month, int day,int year);
 int convert_day(int month, int day);
 
