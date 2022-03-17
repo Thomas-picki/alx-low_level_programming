fab00d67d3736623c20ce91f30c41ebca23e8a70
Thomas-picki <thomasendalemaw@gmail.com>
2022-03-17 22:25:37 +0330

    zero task commit

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void posistive_or_negative(int i);
+int largest_number(int a, int , int c);
+void print_remaining(int month, int day,int year);
+int convert_day(int month, int day);
+
+#endif
