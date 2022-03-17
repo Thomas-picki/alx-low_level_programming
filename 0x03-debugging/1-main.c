1db659e2a5475683a2bbbab5643145fa6065043b
Thomas-picki <thomasendalemaw@gmail.com>
2022-03-17 22:33:13 +0330

    first task commit

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - causes an infinite 1000
+*
+* Return: 0
+*/
+int main(void)
+{
+int i;
+
+printf("infinite loop incoming :(\n")[D[D)");
+
+i =0 ;
+/*
+* while (i<10)
+*{
+*Infinite loop - No increase of varriable
+*putchar(i);
+}
+*/
+
+printf("Infinite loop avoided \\o/\n");
+return (0);
+}
