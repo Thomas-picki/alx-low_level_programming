4f0ea7f857fc235b9fc4919bc28efa6bbdcfc801
Thomas-picki <thomasendalemaw@gmail.com>
2022-03-18 22:33:47 +0330

    4th task commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_most_numbers - pritn numbers
+*
+* Return: always 0
+*/
+void print_most_numbers(void)
+{
+int i;
+int =0;
+while (i<10)
+{
+if (i !=2 && i !=4)
+{
+_putchar (i + '0');
+}
+i++;
+}
+_putchar ('\n');
+}
