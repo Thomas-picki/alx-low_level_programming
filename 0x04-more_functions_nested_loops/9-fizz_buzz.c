8ee05111b8b0f790cc5b3d63e6b2aff3bc023819
Thomas-picki <thomasendalemaw@gmail.com>
2022-03-18 23:03:04 +0330

    9th commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Return: void
+*/
+
+int main(void)
+{
+
+int p = 100;
+int i;
+
+i = 1;
+while (i <= p)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz")[D[D ");
+}
+else if ( i % 3 == 0)
+{
+printf("Fizz ");
+}
+else 
+{
+[A[3~[3[A[C[C[C[C if (i % 5 == 0)
+{
+if (i < p)
+printf("Buzz ");
+
+else 
+{
+printf("Buzz");
+}
+[A[A[A[3~[3~[[B[B[B[A[Belse
+{
+printf("%i ", i);
+}
+
+i++;
+
+}
+printf("\n");
+return (0);
+}
