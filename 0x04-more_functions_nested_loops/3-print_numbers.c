8558b42391f7fb878c1858b8120b92bc6a5b42f9
Thomas-picki <thomasendalemaw@gmail.com>
2022-03-18 22:26:25 +0330

    3rd task commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <main.h[D[D[D[D[D["[C[C[C[C[C[C"
+/** 
+* print_numbers - print numbers
+* Return: always 0
+*/
+void print_numbers(void)
+{
+int i;
+i=0;
+while (i < 10)
+{
+_putchar (i + '0');
+i++
+}
+_putchar ('\n');
+}
