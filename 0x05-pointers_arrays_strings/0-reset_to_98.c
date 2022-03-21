e0a5ac8b0ec17322b7a4e475631afe3605a1c731
Thomas-picki <thomasendalemaw@gmail.com>
2022-03-21 22:23:31 +0330

    task zero reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,19 @@
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code 
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int n;
+
+    n = 402;
+    printf("n=%d\n", n);
+    reset_to_98(&n);
+    printf("n=%d\n", n);
+    return (0);
+}[A[A[A[A[A[C[C[C[C[C[C[C[C[C98[3~[3~[C;[B[B[B[B[B[B[B[B[B
