d9dd8ba6dd1237622c6ce74a4d32ce23dccd6397
Thomas-picki <thomasendalemaw@gmail.com>
2022-03-22 22:14:26 +0300

    1st task commit

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,35 +1,18 @@
-#include "main.h"
-#include <stdio.h>
+#include "main.c"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-void swap_int(int *a, int *b);
-int main(void)
-{
-    int a = 98;
-    int b = 42;
-
-    printf("a = %d\n", a);
-    printf("b = %d\n\n", b);
-
-    swap(&a, &b);    //passing address of m and n to the swap function
-    printf("After Swapping:\n\n");
-    printf("a = %d\n", a);
-    printf("b = %d", b);
-    return 0;
-}
-
-/*
-    pointer 'a' and 'b' holds and 
-    points to the address of 'm' and 'n'
+* main - swap
+*
+* Return: void
 */
-void swap_int(int *a, int *b) 
+
+void swap_int(int *a, int *b)
 {
-    int temp;
-    temp = *a;
-    *a = *b;
-    *b = temp;
+int f;
+
+f = *a;
+*a = *b;
+*b = f;
 }
+git add .
+[A[C[C[C[C[C[C
