9ecccaa59b56a344114dff548dfc530476db8ec3
Thomas-picki <thomasendalemaw@gmail.com>
2022-03-22 22:25:03 +0300

    2nd task commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/** 
+* _strlen- length of a string 
+* @s: a pointer to an int that will be changed
+*
+* Return: void that means our answer
+*/
+
+int _strlen(char *s)
+{
+int x;
+x = 0;
+while(s(x) != '\0')
+{
+x++
+}
+return (x);
+}
