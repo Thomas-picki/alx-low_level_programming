5464e055b2d95abb65400bc8ee6b3ba5c7dfa00b
Thomas-picki <thomasendalemaw@gmail.com>
2022-03-24 22:12:50 +0300

    zero task commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,30 +1,32 @@
+#include "holberton.h"
 
-
-
-#include "main.h"
 /**
-* _strcat - concatnet two strings
-* @dest: a pointer charachter tha will be changed
-* @src: a pointer to a character that will be chaned
-* Return: dest
-*/
-[3~[[B[B[B[B[A[A[A[A[A[A[[B[B[B[B[B[B[B[C[C
+ * *_strcat - function commute srtings
+ * @dest: param pointer to a char
+ * @src: param pointer to a char
+ * Return: return value of dest
+ */
 
-char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
-int i,j;
-i=0;
-while (dest[i] != '\0')
-{
-i++;
-}
-j=0;
-while (src[j] !='\0')
-{
-dest[i] = src[j];
-j++;
-i++;
-}
-dest[i]='\0';
-return (dest);
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
 }
