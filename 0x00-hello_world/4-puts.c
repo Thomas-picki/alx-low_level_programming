e92436e72e664e82422e9f481f4e5c74a1bf5c53
Thomas-picki <thomasendalemaw@gmail.com>
2022-06-02 07:08:45 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - write '"programming is like building a multilingual puzzel'
+ * main - prints string output
  *
  * Return: always 0 (success)
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
