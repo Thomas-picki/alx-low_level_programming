7d58817cd0560d5a5db7be49feba3e6e6f39cfcd
Thomas-picki <thomasendalemaw@gmail.com>
2022-06-02 12:36:26 -0700

    hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Printsstring outputs 
+ * main - Print string outputs 
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-		return (0);
+	return (0);
 }
