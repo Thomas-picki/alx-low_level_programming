f4254dd4120c2dd1a215c97d77119c72c6274e40
Thomas-picki <thomasendalemaw@gmail.com>
2022-06-03 14:58:00 -0700

    print number combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,29 +1,30 @@
-#include <stdio.h>                                              
+#include <stdio.h>
 
-/**                                                              
+/**
  *
- * main - Entry point                                            
- * Return: always 0                                               */                                                              
+ * main - Entry point
+ *
+ * Return: always 0
+ */                                                              
 
-int main(void)                                                   
+int main(void)
 {
-       int t,o;
+	int t,o;
+
        for (t = 0; t <= 9; t++)
        {
-	       for (o = t + 1; o <= 9; o++)                     
-                {
-   			putchar(t + '0');                        
-                        putchar(o + '0');
-		    	if (t < 8)                               
-                        {
-			     	putchar(',');                    
-                                putchar(' ');                    
-                        }                                        
-                }                                                
-								        }                                                        
-
-			        putchar('\n');                                           
-
-				        return (0);                                              
+	       for (o = t + 1; o <= 9; o++)
+	       {
+   			putchar(t + '0');
+			putchar(o + '0');
+		    	if (t < 8)
+			{
+			     	putchar(',');
+				putchar(' ');
+			}
+	       }
+       }
+       putchar('\n');
+       return (0);                                              
 
 }
