8266e2854094bd7cc37f95417e862c8dbe8df07e
Thomas-picki <thomasendalemaw@gmail.com>
2022-06-07 12:03:48 -0700

    time table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,12 +10,12 @@ void print_times_table(int n)
 {
 	int a, b, c;
 
-	if (n >= 0 && n <=15)
+	if (n >= 0 && n <= 15)
 	{
 		for (a = 0; a <= n; a++)
 		{
 			_putchar(48);
-			for (b = 1; b <=n; b++)
+			for (b = 1; b <= n; b++)
 			{
 				c = a * b;
 				_putchar(44);
