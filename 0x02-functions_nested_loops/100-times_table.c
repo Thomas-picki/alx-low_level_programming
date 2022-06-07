560a7309ea3429377a735043b2f1d19b06b49b85
Thomas-picki <thomasendalemaw@gmail.com>
2022-06-07 12:21:50 -0700

    time table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * prinnt times tale- print the time table
+ * prinnt_times_tale - print the time table
  *
  * @n: number time table (0 <n <=15)
  *
@@ -14,29 +14,41 @@ void print_times_table(int n)
 	{
 		for (a = 0; a <= n; a++)
 		{
-			_putchar(48);
+			/*_putchar(48);*/
 			for (b = 1; b <= n; b++)
 			{
 				c = a * b;
-				_putchar(44);
-				_putchar(32);
-				if (c <= 9)
+				/*_putchar(44);
+				_putchar(32);*/
+				if (b == 0)
 				{
-					_putchar(32);
-					_putchar(32);
-					_putchar(c + 48);
+					/*_putchar(32);
+					_putchar(32);*/
+					_putchar(c + '0');
 				}
-				else if (c <= 99)
+				else if (c <= 9)
 				{
-					_putchar(32);
-					_putchar((c / 10) + 48);
-					_putchar((c / 10) + 48);
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(c + '0');
+				}
+				else if (c >= 10 && c < 100)
+				{
+					_putchar(' , ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(c / 10 + '0');
+					_putchar(c % 10 + '0');
 				}
 				else
 				{
-					_putchar(((c / 10) % 10) + 48);
-					_putchar(((c / 10) % 10) + 48);
-					_putchar((c % 10) + 48);
+					_putchar(',');
+					_putchar(' ');
+					_putchar((c / 100) + '0');
+					_putchar(c /10 % 10 + '0');
+					_putchar(c % 10 + '0');
 				}
 			}
 			_putchar('\n');
