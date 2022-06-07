436f9d7351c2e35b6611c14ce7e3972bcb1ba51a
Thomas-picki <thomasendalemaw@gmail.com>
2022-06-07 13:00:14 -0700

    fibonacci numers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 	int x, y;
-	long n1, n2, nx;
+	long int  n1, n2, nx;
 
 	n1 = 1;
 	n2 = 2;
@@ -17,17 +17,16 @@ int main(void)
 	{
 		if (x == 49)
 		{
-			printf(("%ld"); n1));
+			printf("%ld, ", n1);
 		}
 		else
 		{
-			printf("%ld,", n1);
+			printf("%ld\n", n1);
 			nx = n1 + n2;
 			n1 = n2;
 			n2 = nx;
 		}
 	}
-	printf('\n');
 	return (0);
 }
 
