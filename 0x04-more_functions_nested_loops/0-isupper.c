d328f158a6685d90417a1043e69b4dab4ae80343
Thomas-picki <thomasendalemaw@gmail.com>
2022-06-09 12:17:57 -0700

    check uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,19 +7,8 @@
 */
 int _isupper(int c)
 {
-<<<<<<< HEAD
 	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
-=======
-if (c >= 'A' && c <= 'Z')
-
-return (1);
-
-else
-
-return (0);
-
->>>>>>> 500b6e8a76d2a1c847306e92611fb51c3ee5ade1
 }
