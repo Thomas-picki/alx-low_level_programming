836498aa248d67a716cd66bbcc5d109e1fe2e035
Thomas-picki <thomasendalemaw@gmail.com>
2022-06-13 11:29:16 -0700

    pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,17 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 
-/*/*
-/*/*
-
-[A[A[C[[A[B[A[C[C/**
-* rese to 98- update a integer value to 98
-* @n: a pointer to an int that will update
-*
-* Return: void that means correct
-*/
-
+/**
+ * main - check the code
+ * return: always 0
+ */
 void reset_to_98(int *n)
 {
-*n = 98;
+	*n = 98;
 }
