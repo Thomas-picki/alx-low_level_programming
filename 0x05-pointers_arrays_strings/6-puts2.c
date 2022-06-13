a08701f6d3e97fe45f30279f881754a9a1bb1ca8
Thomas-picki <thomasendalemaw@gmail.com>
2022-06-13 12:42:21 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - prints evry other cha of string
+ * @str: input string
+ * Return: void
+ * 
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\n')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+
+		i++;
+	}
+	_putchar('\n');
+}
