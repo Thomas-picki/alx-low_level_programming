2678b9da6dba340521766ec74b449a2ea506a562
Thomas-picki <thomasendalemaw@gmail.com>
2022-06-15 11:31:57 -0700

    0x06

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,20 +11,17 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	unsigned int i = 0;
+	int i = 0;
 	int j = 0;
 
-	while (*(dest + i) != '\0')
+	while (dest[i] != '\0')
 		i++;
-	while (*(src + j) != '\0')
+	while (src[j] != '\0' && n != j)
 	{
-		if (j < n)
-		{
-			*(dest + i) = *(src + j);
-			i++;
-		}
+		*(dest + i) = src[j];
+		i++;
 		j++;
 	}
+	*(dest + i) = '\0';
 	return (dest);
 }
-© 2022 GitHub, Inc.
