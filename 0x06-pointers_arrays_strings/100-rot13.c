1cf8d394ae39b820675fbc946201084ffabd69dc
Thomas-picki <thomasendalemaw@gmail.com>
2022-06-16 11:49:14 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,23 +6,23 @@
  */
 char *rot13(char *s)
 {
-	int i;
+	int i = 0, j;
 
-	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-	char *ptr = s;
+	char rot13[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	char ROT13[] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q',  'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+	
 
-	while (*s)
+	while (s[i])
 	{
-		for (i = 0; i <= 52; i++)
+		for (j = 0; j <= 52; j++)
 		{
-			if (*s == rot13[i])
+			if (s[i] == rot13[j])
 			{
-				*s = ROT13[i];
+				s[i] = ROT13[j];
 				break;
 			}
 		}
-		s++
+		i++
 	}
-	return (ptr);
+	return (s);
 }
