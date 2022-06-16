4d2b5878fc23f640bbe355749f4a85cef39a1f96
Thomas-picki <thomasendalemaw@gmail.com>
2022-06-16 12:18:57 -0700

    magic

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+	/**
+	 * write line of code
+	 * remember
+	 * not allowed a
+	 * not allowd p
+	 * only one statement
+	 */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
