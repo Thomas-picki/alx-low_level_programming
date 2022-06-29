b0ae48e8b74775ed86bea8c8f2293f571be88852
Thomas-picki <thomasendalemaw@gmail.com>
2022-06-29 01:27:27 -0700

    Create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,7 +18,7 @@ char *create_array(unsigned int size, char c)
 	if (arr == NULL)
 		return ('\0');
 	for (i = 0; i < size; i++)
-		arr[i] = c;
-	arr[size] = '\0';
+		*arr[i] = c;
+	*arr[i] = '\0';
 	return (arr);
 }
