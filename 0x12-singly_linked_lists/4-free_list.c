203e0f61adb416f9c3c449bf9fd5f7fe0a092f0d
Thomas-picki <thomasendalemaw@gmail.com>
2022-07-14 11:23:56 -0700

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_list - frees a list 
+ * @head: pointer to head
+ */
+void free_list(list_t *head)
+{
+	list_t *place_holder;
+
+	while (head)
+	{
+		if (head->str)
+			free(head->str);
+		place_holder = head;
+		head = head->next;
+
+		free(place_holder);
+	}
+}
